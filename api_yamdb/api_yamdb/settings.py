c9096384062d4b6c77ce8afa16cb9d0ca9fb58b3
Roman Glebov <kitaro630@yandex.ru>
2023-05-24 22:09:13 +0300

    resolve conflict

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,8 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]

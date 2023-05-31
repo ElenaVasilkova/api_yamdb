788b1a9c1f34ba5fc74a81dad572aca5ff495e6a
ElenaVasilkova <117449567+ElenaVasilkova@users.noreply.github.com>
2023-05-31 16:52:01 +0300

    Update import

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-from reviews.models import Category, Genre, Title, User, Comment, Review
+from reviews.models import Category, Genre, Review, Title, User
 
 from .permissions import (Admin, AdminModeratorAuthorPermission,
                           IsAdminUserOrReadOnly)
